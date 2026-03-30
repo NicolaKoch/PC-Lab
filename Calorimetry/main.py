cbad8899a9932c691456d4a5a442841149267a50
Nicola Koch <76223559+NicolaKoch@users.noreply.github.com>
2026-03-30 13:00:02 +0200

    Update main.py

--- Calorimetry/main.py
+++ Calorimetry/main.py
@@ -4,42 +4,38 @@
 # 3. Make 2 functions, one for normal plot and one for normalized plot
 # 4. Call the functiuons to display plots 
 
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+DATA_DIR = os.path.dirname(__file__)
+
 #Import data
-data1 = np.loadtxt('Data1.csv', delimiter=',', skiprows=1)
-data2 = np.loadtxt('Data2.csv', delimiter=',', skiprows=1)
-data3 = np.loadtxt('Data3.csv', delimiter=',', skiprows=1)
-data4 = np.loadtxt('Data4.csv', delimiter=',', skiprows=1)
-data5 = np.loadtxt('Data5.csv', delimiter=',', skiprows=1)
-data6 = np.loadtxt('Data6.csv', delimiter=',', skiprows=1)
+data1 = np.loadtxt(os.path.join(DATA_DIR, 'Data1.csv'), delimiter=',', skiprows=1)
+data2 = np.loadtxt(os.path.join(DATA_DIR, 'Data2.csv'), delimiter=',', skiprows=1)
+data3 = np.loadtxt(os.path.join(DATA_DIR, 'Data3.csv'), delimiter=',', skiprows=1)
+data4 = np.loadtxt(os.path.join(DATA_DIR, 'Data4.csv'), delimiter=',', skiprows=1)
+data5 = np.loadtxt(os.path.join(DATA_DIR, 'Data5.csv'), delimiter=',', skiprows=1)
+data6 = np.loadtxt(os.path.join(DATA_DIR, 'Data6.csv'), delimiter=',', skiprows=1)
 # List of Datasets
 ListOfData = [data1, data2, data3, data4, data5, data6]
-def normalPlot(ListOfData):
+ListOflabels = ['Benzoic acid', 'Benzoic acid', 'Fumaric acid', 'Maleic acid', 'Fumaric acid', 'Maleic acid']
+def normalPlot(ListOfData, ListOflabels):
     plt.figure(figsize=(10, 6))
-    plt.plot(data1[:, 0], data1[:, 1], label='Benzoic acid', marker='o')
-    plt.plot(data2[:, 0], data2[:, 1], label='Benzoic acid', marker='s')
-    plt.plot(data3[:, 0], data3[:, 1], label='Data3', marker='^')
-    plt.plot(data4[:, 0], data4[:, 1], label='Data4', marker='d')
-    plt.plot(data5[:, 0], data5[:, 1], label='Data5', marker='x')
-    plt.plot(data6[:, 0], data6[:, 1], label='Data6', marker='v')
-
-
+    for data, label in zip(ListOfData, ListOflabels):
+        plt.plot(data[:, 0], data[:, 1], label=label, marker='o')
     plt.xlabel('Time [s]')
     plt.ylabel('Temperature [°C]')
     plt.title('Plot of Temperature over Time')
     plt.legend()
     plt.grid()
-    plt.show()  
+    plt.show()
 
 
-def normalizedPlot(ListOfData):
-    # Normalize the data by subtracting the first temperature value from all temperature values
+def normalizedPlot(ListOfData, ListOflabels):
     plt.figure(figsize=(10, 6))
-    for i, data in enumerate(ListOfData):
-        normalized_temperature = data[:, 1] - data[0, 1]
-        plt.plot(data[:, 0], normalized_temperature, label=f'Data{i+1}', marker='o')
+    for data, label in zip(ListOfData, ListOflabels):
+        plt.plot(data[:, 0], data[:, 1] - data[0, 1], label=label, marker='o')
 
     # Plot the normailzed data like before
     plt.xlabel('Time [s]')
@@ -49,5 +45,5 @@ def normalizedPlot(ListOfData):
     plt.grid()
     plt.show()
 
-normalPlot(ListOfData)
-normalizedPlot(ListOfData)
+normalPlot(ListOfData, ListOflabels)
+normalizedPlot(ListOfData, ListOflabels)
